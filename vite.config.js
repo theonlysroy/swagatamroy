c35fe8396d9752cbb92fdc11efd501c8262addf0
theonlysroy <tirtha13july2002sonofmp@gmail.com>
2025-01-19 20:13:58 +0530

    base url vite config changes

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import { nodePolyfills } from "vite-plugin-node-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/swagatamroy/",
   plugins: [
     react(),
     nodePolyfills({
       include: ["path", "fs"],
-      overrides: {
-      },
+      overrides: {},
     }),
   ],
   resolve: {
